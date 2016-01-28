3718dc036dc894979694cc9eee7ef85a3b6c50cd
mockinjay <ciobanu.casian94@gmail.com>
2016-01-28 16:02:41 +0200

    NewMethods

---
+++ DataLayer/BussinessLayer/Bussiness.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Models;
+using System.Data.Entity;
+
+namespace BussinessLayer
+{
+    public class BussinessTier
+    {
+        private DataTier dt = new DataTier();
+
+        public List<Facultati> readFaculties()
+        {
+            List<Facultati> fac = new List<Facultati>();
+            IDbSet<Facultati> facultati = dt.readFaculties();
+            foreach(var temp in facultati)
+            {
+                fac.Add(temp);
+            }
+            return fac;
+        }
+        public string checkUser(string email, string pass)
+        {
+            return dt.checkUser(email, pass);
+        }
+    }
+}
